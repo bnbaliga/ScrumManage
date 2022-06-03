1bc9b9c33476f6f83460a8189e03900d9dc9d3ae
Narasimha Baliga <bnbaliga@gmail.com>
2022-06-03 14:52:22 -0500

    Initial Commit

---
+++ ScrumManage/ScrumManageApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumManageApp.swift
+//  ScrumManage
+//
+//  Created by Narasimha Baliga on 6/3/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumManageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
